9fbc39898a04fd36167f0b59eec8f647b18b5984
DhairyaShah2008 <81810113+DhairyaShah2008@users.noreply.github.com>
2021-05-24 18:03:44 +0530

    Add files via upload

---
+++ js/chain.js
@@ -0,0 +1,37 @@
+class Slingshot{
+    constructor(A,pointB) {
+        var options = {
+   bodyA:A,
+pointB:pointB,
+//property:arguement
+   stiffness:0.003,
+   length:1
+   
+        }
+        
+           
+        this.pointB=pointB
+this.chain=Constraint.create(options)
+World.add(world,this.chain)
+
+
+      }
+      fly(){
+          this.chain.bodyA=null
+    }
+
+    attach(body){
+        this.chain.bodyA=body
+        }
+      display(){
+          if(this.chain.bodyA){
+            var APos=this.chain.bodyA.position
+            var BPos=this.pointB
+  strokeWeight(5)
+  line(APos.x,APos.y,BPos.x,BPos.y)
+          }
+         
+      }
+
+      
+}
